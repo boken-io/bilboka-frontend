74b309a2136362efcae0a1ee369b2510e9017fd7
John Føreland <john.foreland@me.com>
2023-12-28 15:03:43 +0100

    removed vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import { sql } from '@vercel/postgres';
 import { Card, Title, Text } from '@tremor/react';
 import Search from './search';
 import UsersTable from './table';
+
+import prisma from '../lib/prisma'
+
 
 interface User {
   id: number;
@@ -16,12 +18,9 @@ export default async function IndexPage({
   searchParams: { q: string };
 }) {
   const search = searchParams.q ?? '';
-  const result = await sql`
-    SELECT id, name, username, email 
-    FROM users 
-    WHERE name ILIKE ${'%' + search + '%'};
-  `;
-  const users = result.rows as User[];
+  const result = await prisma.users.findMany()
+
+  const users = result as User[];
 
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
