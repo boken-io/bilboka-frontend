8e1a3e1dd5c71cf8ac2692208741b697ba2a3884
John Føreland <john.foreland@me.com>
2023-12-30 23:24:37 +0100

    moved components out of app folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 
 import Nav from './navbar/nav';
-import Toast from './toast';
+import Toast from '../components/toast/toast';
 import { Suspense } from 'react';
 
 export const metadata = {
