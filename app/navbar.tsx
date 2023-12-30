d3fe58de05a41359154e6d5c456eb27af43273be
John Føreland <john.foreland@me.com>
2023-12-30 12:38:15 +0100

    separated user login

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -16,6 +16,76 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
+function loggedInUser(user: any) {
+  //   if (user === undefined) return;
+  return (
+    <div className="hidden sm:ml-6 sm:flex sm:items-center">
+      <Menu as="div" className="relative ml-3">
+        <div>
+          <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
+            <div className="grid grid-flow-col auto-cols-max gap-4">
+              <div className="place-self-center place-self-end">
+                {user?.name}
+              </div>
+              <div className="place-self-end">
+                <span className="sr-only">Open user menu</span>
+                <Image
+                  className="h-8 w-8 rounded-full"
+                  src={user?.image || 'https://avatar.vercel.sh/leerob'}
+                  height={32}
+                  width={32}
+                  alt={`${user?.name || 'placeholder'} avatar`}
+                />
+              </div>
+            </div>
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-200"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            {user ? (
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={classNames(
+                      active ? 'bg-gray-100' : '',
+                      'flex w-full px-4 py-2 text-sm text-gray-700'
+                    )}
+                    onClick={() => signOut()}
+                  >
+                    Sign out
+                  </button>
+                )}
+              </Menu.Item>
+            ) : (
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={classNames(
+                      active ? 'bg-gray-100' : '',
+                      'flex w-full px-4 py-2 text-sm text-gray-700'
+                    )}
+                    onClick={() => signIn('github')}
+                  >
+                    Sign in
+                  </button>
+                )}
+              </Menu.Item>
+            )}
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+}
+
 export default function Navbar({ user }: { user: any }) {
   const pathname = usePathname();
 
@@ -67,72 +137,7 @@ export default function Navbar({ user }: { user: any }) {
                   ))}
                 </div>
               </div>
-              <div className="hidden sm:ml-6 sm:flex sm:items-center">
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
-                      <div className="grid grid-flow-col auto-cols-max gap-4">
-                        <div className="place-self-center place-self-end">
-                          {user?.name}
-                        </div>
-                        <div className="place-self-end">
-                          <span className="sr-only">Open user menu</span>
-                          <Image
-                            className="h-8 w-8 rounded-full"
-                            src={
-                              user?.image || 'https://avatar.vercel.sh/leerob'
-                            }
-                            height={32}
-                            width={32}
-                            alt={`${user?.name || 'placeholder'} avatar`}
-                          />
-                        </div>
-                      </div>
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      {user ? (
-                        <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'flex w-full px-4 py-2 text-sm text-gray-700'
-                              )}
-                              onClick={() => signOut()}
-                            >
-                              Sign out
-                            </button>
-                          )}
-                        </Menu.Item>
-                      ) : (
-                        <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'flex w-full px-4 py-2 text-sm text-gray-700'
-                              )}
-                              onClick={() => signIn('github')}
-                            >
-                              Sign in
-                            </button>
-                          )}
-                        </Menu.Item>
-                      )}
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
+              {loggedInUser(user)}
               <div className="-mr-2 flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
                   <span className="sr-only">Open main menu</span>
