ecfb5f796e8bfacfd27631cd9630ca913b64befd
John Føreland <john.foreland@me.com>
2023-12-30 23:17:59 +0100

    removed unusued info from index

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,10 @@ import Search from './search';
 import UsersTable from './table';
 import LoginOptions from './login';
 
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-}
-
-export default async function IndexPage({
-  searchParams
-}: {
-  searchParams: { q: string };
-}) {
-  const users = [] as User[];
-
+export default async function IndexPage() {
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <LoginOptions></LoginOptions>
-      <Text>A list of users retrieved from a Postgres database.</Text>
-      <Search />
-      <Card className="mt-6">
-        <UsersTable users={users} />
-      </Card>
     </main>
   );
 }
