bf259d1a8560bb496df2a2215f5c0cbc836a32bc
John Føreland <john.foreland@me.com>
2023-12-31 00:35:53 +0100

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -29,21 +29,21 @@ export default function Page() {
             <div className="">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
-                for="email"
+                htmlFor="email"
               >
-                Username
+                Email
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="email"
-                type="text"
+                type="email"
                 placeholder="Email"
               />
             </div>
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
-                for="message"
+                htmlFor="message"
               >
                 Message
               </label>
