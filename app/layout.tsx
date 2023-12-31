775fdccec0e6d9f532b58366a72168c4e92e3a01
John Føreland <john.foreland@me.com>
2023-12-31 12:04:35 +0100

    renamed html title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import Toast from './_components/toast/toast';
 import { Suspense } from 'react';
 
 export const metadata = {
-  title: 'Next.js App Router + NextAuth + Tailwind CSS',
-  description:
-    'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, ESLint, and Prettier.'
+  title: 'Bilboken',
+  description: 'Administrer drivstoff-priser.'
 };
 
 export default function RootLayout({
