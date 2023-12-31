afa3cdb6fa49c4d5ba1a4526fc0439fcd0c31e2f
John Føreland <john.foreland@me.com>
2023-12-31 14:23:56 +0100

    update formatting on delete page

--- app/delete/page.tsx
+++ app/delete/page.tsx
@@ -5,24 +5,33 @@ import { FormEvent } from 'react';
 
 export default function Page() {
   return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Card>
+    <main className="p-4 md:p-10 mx-auto max-w-5xl">
+      <Card className="px-12">
+        <h1 className="flow text-5xl font-extrabold mb-5">
+          Delete Your Account
+        </h1>
         <p>
-          Welcome to Boken.io! We understand that circumstances may lead you to
-          consider deleting your account, and we want to make this process as
-          straightforward as possible. Please follow the instructions below to
-          initiate the account deletion request:
+          We understand that circumstances may lead you to consider deleting
+          your account, and we want to make this process as straightforward as
+          possible. Please follow the instructions below to initiate the account
+          deletion request:
         </p>
 
         <br />
-        <h3 className="text-xl">Steps to Delete Your Account</h3>
-        <p>
-          1. Go to{' '}
-          <a href="https://bil.boken.io/contact" target="_blank">
-            https://bil.boken.io/contact
-          </a>
-        </p>
-        <p>2. Provide Necessary Information</p>
+        <h3 className="text-3xl mb-4">Steps to Delete Your Account</h3>
+        <ul
+          role="list"
+          className="marker:text-gray-600 list-decimal ps-5 space-y-2 text-sm"
+        >
+          <li>
+            Go to the{' '}
+            <a className="underline" href="/contact" target="_blank">
+              contact page
+            </a>
+          </li>
+          <li>Provide Necessary Information</li>
+          <li>You will receive further information via email</li>
+        </ul>
 
         <br />
         <p>Thank you for choosing Boken.io!</p>
