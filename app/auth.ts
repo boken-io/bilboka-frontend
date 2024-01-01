16f93dbe04c3bc1c97be098b0ba1a342c26a2feb
John Føreland <john.foreland@me.com>
2024-01-01 14:29:44 +0100

    use openid for facebook login

--- app/auth.ts
+++ app/auth.ts
@@ -8,7 +8,38 @@ export const {
   handlers: { GET, POST },
   auth
 } = NextAuth({
-  providers: [GitHub, AppleProvider, GoogleProvider, FacebookProvider],
+  providers: [
+    GitHub,
+    AppleProvider,
+    GoogleProvider,
+    FacebookProvider({
+      idToken: true,
+      clientId: process.env.AUTH_FACEBOOK_ID,
+      clientSecret: process.env.AUTH_FACEBOOK_SECRET,
+      authorization: {
+        url: 'https://www.facebook.com/v11.0/dialog/oauth',
+        params: {
+          client_id: process.env.AUTH_FACEBOOK_ID,
+          scope: 'openid email',
+          response_type: 'code'
+        }
+      },
+      token: {
+        url: 'https://graph.facebook.com/oauth/access_token',
+        async request(context) {
+          const url =
+            `https://graph.facebook.com/oauth/access_token` +
+            `?code=${context.params.code}` +
+            `&client_id=${context.provider.clientId}` +
+            `&redirect_uri=${context.provider.callbackUrl}` +
+            `&client_secret=${context.provider.clientSecret}`;
+          const response = await fetch(url);
+          const tokens = await response.json();
+          return { tokens };
+        }
+      }
+    })
+  ],
   pages: {
     signIn: '/sign-in'
   }
