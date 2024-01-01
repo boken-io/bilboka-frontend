4b9b15885972fc93d758308495f7bc9d2fa2066f
John Føreland <john.foreland@me.com>
2024-01-01 23:06:09 +0100

    clickable vehicles

--- app/vehicles/page.tsx
+++ app/vehicles/page.tsx
@@ -1,6 +1,7 @@
 import { Card, Metric, Text, Title, BarList, Flex, Grid } from '@tremor/react';
 import { FetchVehicles, Vehicles } from './fetch';
 import CheckIfAuthenticated from '../_components/check-auth';
+import { redirect } from 'next/navigation';
 
 export default async function Page() {
   await CheckIfAuthenticated();
@@ -10,32 +11,34 @@ export default async function Page() {
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
-        {vehicles.map((item) => (
-          <Card key={item.tegnkombinasjon}>
-            <Title>{item.tegnkombinasjon}</Title>
+        {vehicles.map((vehicle) => (
+          <a
+            key={vehicle.tegnkombinasjon}
+            href={`/vehicles/${vehicle.id}`}
+            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 "
+          >
+            <Title>{vehicle.tegnkombinasjon}</Title>
             <Flex
               justifyContent="start"
               alignItems="baseline"
               className="space-x-2"
             >
-              <Metric>{item.name}</Metric>
+              <Metric>{vehicle.name}</Metric>
             </Flex>
             <Flex
               justifyContent="end"
               alignItems="baseline"
               className="space-x-2"
             >
-              <Metric>{item.tankVolume}</Metric>
-              <Text>{item.odometerUnit}</Text>
+              <Metric>{vehicle.tankVolume}</Metric>
+              <Text>{vehicle.odometerUnit}</Text>
             </Flex>
             <Flex
-              justifyContent="start"
+              justifyContent="center"
               alignItems="baseline"
               className="space-x-2"
-            >
-              {/* <button onClick={redirect('/vehicles/' + item.id)}></button> */}
-            </Flex>
-          </Card>
+            ></Flex>
+          </a>
         ))}
       </Grid>
     </main>
