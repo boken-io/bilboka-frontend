4303cafd57cd6bee495223b2f30ceb4d7a8ba303
John Føreland <john.foreland@me.com>
2024-01-01 16:49:34 +0100

    added loading spinner

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default async function IndexPage() {
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <LoginOptions></LoginOptions>
-      <LoadingSpinner></LoadingSpinner>
     </main>
   );
 }
