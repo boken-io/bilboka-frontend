09bda1c3f66087087609be7f145b4e57e80e6b0d
John Føreland <john.foreland@me.com>
2024-01-07 12:38:03 +0100

    removed toast from backend

--- app/vehicles/fetch.tsx
+++ app/vehicles/fetch.tsx
@@ -1,12 +1,10 @@
 import { Vehicles, Vehicle } from './[vehicle]/model';
-import toast from 'react-hot-toast';
 
 export async function FetchVehicles(): Promise<Vehicles> {
   try {
     const res = await fetch(`${process.env.BILBOKEN_API_URL}/vehicles/sample`);
     return (await res.json()) as Vehicles;
   } catch (err: any) {
-    toast.error(err);
     return [];
   }
 }
@@ -18,7 +16,6 @@ export async function FetchVehicle(vehicleId: string) {
     );
     return await res.json();
   } catch (err: any) {
-    toast.error(err);
     return [];
   }
 }
