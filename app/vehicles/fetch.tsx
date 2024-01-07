64a6797274c5c2b97605eb944215da6f13657be8
John Føreland <john.foreland@me.com>
2024-01-07 12:50:54 +0100

    use sample env variable

--- app/vehicles/fetch.tsx
+++ app/vehicles/fetch.tsx
@@ -2,7 +2,11 @@ import { Vehicles, Vehicle } from './[vehicle]/model';
 
 export async function FetchVehicles(): Promise<Vehicles> {
   try {
-    const res = await fetch(`${process.env.BILBOKEN_API_URL}/vehicles/sample`);
+    const res = await fetch(
+      `${process.env.BILBOKEN_API_URL}/vehicles${
+        process.env.USE_SAMPLE ? '/sample' : ''
+      }`
+    );
     return (await res.json()) as Vehicles;
   } catch (err: any) {
     return [];
@@ -13,6 +17,17 @@ export async function FetchVehicle(vehicleId: string) {
   try {
     const res = await fetch(
       `${process.env.BILBOKEN_API_URL}/vehicles/${vehicleId}/sample`
+    );
+    return await res.json();
+  } catch (err: any) {
+    return [];
+  }
+}
+
+export async function FetchEntries(vehicleId: string) {
+  try {
+    const res = await fetch(
+      `${process.env.BILBOKEN_API_URL}/vehicles/${vehicleId}/entries/sample`
     );
     return await res.json();
   } catch (err: any) {
