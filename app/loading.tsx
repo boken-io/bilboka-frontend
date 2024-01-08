37088e47035a91a2b0438884b89daf7be95dbe00
John Føreland <john.foreland@me.com>
2024-01-08 21:28:43 +0100

    fixed loading spinner not needing global.css

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,9 @@
 import LoadingSpinner from '../components/loading-spinner/loading-spinner';
 
 export default function Loading() {
-  return <LoadingSpinner></LoadingSpinner>;
+  return (
+    <div className="h-3/5 grid justify-items-center content-center loader">
+      <LoadingSpinner></LoadingSpinner>
+    </div>
+  );
 }
