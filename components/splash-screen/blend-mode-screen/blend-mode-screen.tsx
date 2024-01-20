6398e97080abaa19f58aed093738faaefcf00dc5
John Føreland <john.foreland@me.com>
2024-01-20 18:20:48 +0100

    new video

--- components/splash-screen/blend-mode-screen/blend-mode-screen.tsx
+++ components/splash-screen/blend-mode-screen/blend-mode-screen.tsx
@@ -17,9 +17,13 @@ export default async function BlendMode() {
         style={{
           filter: 'grayscale(100%) contrast(140%) brightness(90%)'
         }}
-        src="/video/blurred.mp4"
+        src="/video/highway2.mp4"
       />
+
       {/* name */}
+      <br />
+      <br />
+      <br />
       <div className="grid gap-4 content-center min-h-screen bg-gradient-to-t from-slate-400 dark:from-slate-800 to-70%">
         <h1
           className="font-bold text-white  text-center text-7xl lg:text-9xl xl:text-9xl 2xl:text-9xl"
