30b4bf70ff21b6dba772d2b101381a35c02f0ffa
John Føreland <john.foreland@me.com>
2024-01-20 17:59:25 +0100

    fixed dark color

--- components/splash-screen/blend-mode-screen/blend-mode-screen.tsx
+++ components/splash-screen/blend-mode-screen/blend-mode-screen.tsx
@@ -15,16 +15,15 @@ export default async function BlendMode() {
         loop
         muted
         style={{
-          filter: 'grayscale(100%) contrast(140%) brightness(90%)',
-          transform: 'scale(1.25) translateX(-10px)'
+          filter: 'grayscale(100%) contrast(140%) brightness(90%)'
         }}
-        src="/video/woods.mp4"
+        src="/video/blurred.mp4"
       />
       {/* name */}
       <div className="grid gap-4 content-center min-h-screen bg-gradient-to-t from-slate-400 dark:from-slate-800 to-70%">
         <h1
-          className="font-bold text-white text-center text-7xl lg:text-9xl xl:text-9xl 2xl:text-9xl"
-          style={{ mixBlendMode: 'difference', color: '#fff' }}
+          className="font-bold text-white  text-center text-7xl lg:text-9xl xl:text-9xl 2xl:text-9xl"
+          style={{ mixBlendMode: 'difference' }}
         >
           Bilboken
         </h1>
--- components/splash-screen/blend-mode-screen/buttons.tsx
+++ components/splash-screen/blend-mode-screen/buttons.tsx
@@ -8,8 +8,8 @@ export default function Buttons({ user }: { user: any }) {
     <>
       {!user && (
         <button
-          className="place-self-center bg-slate-800 hover:bg-slate-900 text-white font-bold py-2 px-4 rounded-full shrink flex-none w-36"
-          style={{ mixBlendMode: 'difference', color: '#fff' }}
+          className="place-self-center bg-white  hover:bg-gray-200 text-black font-bold py-2 px-4 rounded-full shrink flex-none w-36"
+          style={{ mixBlendMode: 'difference' }}
           onClick={() => router.push('?login=true')}
         >
           Logg inn
