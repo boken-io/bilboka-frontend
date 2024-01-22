2c40aff3abb78bcea7e070ee2beb354d9e511c24
John Føreland <john.foreland@me.com>
2024-01-22 19:41:34 +0100

    removed apple tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,10 +41,7 @@ export default function RootLayout({
     <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
+
         <meta name="apple-mobile-web-app-title" content={siteConfig.name} />
       </head>
       <body
