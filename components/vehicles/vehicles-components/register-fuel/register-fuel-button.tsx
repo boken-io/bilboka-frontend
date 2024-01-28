8b6858f9a27e69cdf164ea78a0bb432b1c77e653
John Føreland <john.foreland@me.com>
2024-01-28 18:33:32 +0100

    used plus instead of svg for register

--- components/vehicles/vehicles-components/register-fuel/register-fuel-button.tsx
+++ components/vehicles/vehicles-components/register-fuel/register-fuel-button.tsx
@@ -10,23 +10,14 @@ export default function RegisterFuelButton({
   return (
     <>
       <Link
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full fixed bottom-4 right-4"
+        className="hover:brightness-75 text-white text-2xl font-bold py-2 px-4 rounded-full inline-flex flex-col fixed bottom-4 right-4"
+        style={{
+          backgroundImage:
+            'linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12)'
+        }}
         href="?modal=true"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-          />
-        </svg>
+        {/* TODO make the gradient background a reusable style */}+
       </Link>
 
       {showModal && <RegisterFuel />}
