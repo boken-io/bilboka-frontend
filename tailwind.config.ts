e96fb3769a4d791f8a236e8412b4ef0f44152a4a
John Føreland <john.foreland@me.com>
2024-02-05 18:48:29 +0100

    moved all files to /src folder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,9 @@ const colors = require('tailwindcss/colors');
 
 export default {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@tremor/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
