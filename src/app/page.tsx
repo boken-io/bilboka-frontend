71b6f4e34c0d44b1d354a5b7aa94031370c8c7d0
John Føreland <john.foreland@me.com>
2024-02-06 23:05:15 +0100

    test new corbado plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Valley from '@/components/splash-screen/valley-screen/valley-screen';
 import BlendMode from '@/components/splash-screen/blend-mode-screen/blend-mode-screen';
 import CheckCorbadoAuth from '@/lib/auth/check-corbado-auth';
+import CorbadoLogin from '@/components/auth/corbado-login';
 
 export default async function IndexPage() {
   return (
-    // <Valley />
-    // <BlendMode />
-    <CheckCorbadoAuth />
+    <>
+      {/* <Valley /> */}
+      {/* <BlendMode /> */}
+      <CheckCorbadoAuth />
+      <CorbadoLogin />
+    </>
   );
 }
--- src/components/auth/corbado-login.tsx
+++ src/components/auth/corbado-login.tsx
@@ -1,28 +1,21 @@
 'use client';
-import '@corbado/webcomponent/pkg/auth_cui.css';
 
-import { useEffect, useState } from 'react';
+import { SignUp, Login } from '@corbado/react';
+import { CorbadoProvider } from '@corbado/react';
 
 export default function CorbadoLogin() {
-  const [session, setSession] = useState(null);
-
-  useEffect(() => {
-    // This will run only on client-side
-    import('@corbado/webcomponent');
-  }, []);
-
   return (
-    <corbado-auth
-      project-id={process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID}
-      conditional="yes"
+    <CorbadoProvider
+      projectId={process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID}
+      darkMode="off"
     >
-      <input
-        name="username"
-        id="corbado-username"
-        data-input="username"
-        required
-        autoComplete="webauthn"
-      />
-    </corbado-auth>
+      <SignUp onSignedUp={() => {}} />
+      <Login onLoggedIn={() => {}} />
+      {/* TODO:
+       - Fix dynamic dark mode
+       - translations 
+       - Set provider at layout level
+       */}
+    </CorbadoProvider>
   );
 }
