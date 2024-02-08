6596507dbb6c58f12efa543d2be2487aa09a69ca
John Føreland <john.foreland@me.com>
2024-02-08 17:17:12 +0100

    auth using next-auth with corbado

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,9 +1,12 @@
-import NextAuth from 'next-auth';
+import NextAuth, { User } from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 import AppleProvider from 'next-auth/providers/apple';
 import GoogleProvider from 'next-auth/providers/google';
 import FacebookProvider from 'next-auth/providers/facebook';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import * as jose from 'jose';
+
+type CorbadoInput = { callbackUrl: string; cbo_short_session: string };
 
 export const {
   handlers: { GET, POST },
@@ -18,54 +21,42 @@ export const {
       name: 'Passkeys',
       id: 'corbado',
       credentials: {},
-      async authorize(credentials, req) {
-        console.log('credentials: ' + JSON.stringify(credentials));
-        console.log('req: ' + JSON.stringify(req));
-        console.log('req.headers: ' + JSON.stringify(req.headers));
+      async authorize(
+        credentials: Partial<Record<never, unknown>>,
+        req: Request
+      ): Promise<User | null> {
+        let data = credentials as CorbadoInput;
+        let token = data.cbo_short_session;
 
-        return null;
-        // // Get the token from the cookie
-        // var cbo_short_session = req.headers.cookie
-        //   .split('; ')
-        //   .find((row) => row.startsWith('cbo_short_session'));
-        // var token = cbo_short_session.split('=')[1];
+        // Get the JWKS URL from the project ID
+        var issuer = `https://${process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID}.frontendapi.corbado.io`;
+        var jwksUrl = issuer + '/.well-known/jwks';
 
-        // // Get the JWKS URL from the project ID
-        // var issuer = 'https://' + process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID + '.frontendapi.corbado.io';
-        // var jwksUrl = issuer + '/.well-known/jwks';
+        // Initialize the JWKS client
+        const JWKS = jose.createRemoteJWKSet(new URL(jwksUrl), {
+          cacheMaxAge: 10 * 60 * 1000
+        });
+        const options = {
+          issuer: issuer
+        };
 
-        // // Initialize the JWKS client
-        // const JWKS = jose.createRemoteJWKSet(new URL(jwksUrl), {
-        //   cacheMaxAge: 10 * 60 * 1000
-        // });
-        // const options = {
-        //   issuer: issuer
-        // };
+        const { payload } = await jose.jwtVerify(token, JWKS, options);
+        console.log(JSON.stringify(payload, null, 2));
 
-        // const { payload } = await jose.jwtVerify(token, JWKS, options);
-        // if (payload.iss !== issuer) {
-        //   return null; // Return null if conditions are not met
-        // }
+        if (payload.iss !== issuer) {
+          return null;
+        }
 
-        // return { email: payload.email, name: payload.name, image: null };
+        return {
+          id: payload.sub as string,
+          email: payload.email as string,
+          name: payload.name as string,
+          image: null as null
+        };
       }
     })
   ],
   pages: {
     signIn: '/login'
   }
-  // https://nextjs.org/learn/dashboard-app/adding-authentication
-  // callbacks: {
-  //   authorized({ auth, request: { nextUrl } }) {
-  //     const isLoggedIn = !!auth?.user;
-  //     const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-  //     if (isOnDashboard) {
-  //       if (isLoggedIn) return true;
-  //       return false; // Redirect unauthenticated users to login page
-  //     } else if (isLoggedIn) {
-  //       return Response.redirect(new URL('/dashboard', nextUrl));
-  //     }
-  //     return true;
-  //   },
-  // }
 });
--- src/components/auth/auth-corbado.tsx
+++ src/components/auth/auth-corbado.tsx
@@ -8,7 +8,6 @@ import { Tabs, Tab } from '@nextui-org/react';
 import React, { Key } from 'react';
 import { signIn } from 'next-auth/react';
 import Image from 'next/image';
-import LoginOptions from './login-options';
 
 export type Modes = 'login' | 'register';
 
@@ -19,41 +18,31 @@ export default function CorbadoLogin(props: { mode: Modes }) {
   const [selected, setSelected] = React.useState(props.mode);
 
   function closeModal() {
-    // router.push(path);
-    signIn('facebook');
-    // signIn('corbado', { callbackUrl: path });
+    signIn('corbado', {
+      callbackUrl: path,
+      cbo_short_session: localStorage.getItem('cbo_short_session')
+    });
   }
 
   return (
-    <>
-      <Image
-        className="rounded-full cursor-pointer"
-        onClick={() => closeModal()}
-        src="/auth/apple.webp"
-        height={64}
-        width={64}
-        alt="Apple Login"
-      />
-      <LoginOptions />
-      <CorbadoProvider
-        projectId={process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID}
-        darkMode="off"
-        customTranslations={corbadoTranslations}
+    <CorbadoProvider
+      projectId={process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID}
+      darkMode="off"
+      customTranslations={corbadoTranslations}
+    >
+      <Tabs
+        aria-label="Options"
+        className="justify-center mt-4"
+        selectedKey={selected}
+        onSelectionChange={setSelected as (key: Key) => void}
       >
-        <Tabs
-          aria-label="Options"
-          className="justify-center mt-4"
-          selectedKey={selected}
-          onSelectionChange={setSelected as (key: Key) => void}
-        >
-          <Tab key="login" title="Logg inn">
-            <Login onLoggedIn={closeModal} />
-          </Tab>
-          <Tab key="register" title="Registrer">
-            <SignUp onSignedUp={closeModal} />
-          </Tab>
-        </Tabs>
-      </CorbadoProvider>
-    </>
+        <Tab key="login" title="Logg inn">
+          <Login onLoggedIn={closeModal} />
+        </Tab>
+        <Tab key="register" title="Registrer">
+          <SignUp onSignedUp={closeModal} />
+        </Tab>
+      </Tabs>
+    </CorbadoProvider>
   );
 }
