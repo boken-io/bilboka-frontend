13da8b835f60bd4cee8823048fe33dac62fdb8c8
John Føreland <john.foreland@me.com>
2024-02-08 19:56:13 +0100

    added card to social signon

--- src/components/auth/auth-corbado.tsx
+++ src/components/auth/auth-corbado.tsx
@@ -4,7 +4,7 @@ import corbadoTranslations from '@/config/corbado-translations';
 import { Login, SignUp } from '@corbado/react';
 import { CorbadoProvider } from '@corbado/react';
 import { useRouter, usePathname } from 'next/navigation';
-import { Tabs, Tab } from '@nextui-org/react';
+import { Tabs, Tab, Card, CardBody } from '@nextui-org/react';
 import React, { Key } from 'react';
 import { signIn } from 'next-auth/react';
 import LoginOptions from './login-options';
@@ -25,27 +25,33 @@ export default function CorbadoLogin(props: { mode: Modes }) {
   }
 
   return (
-    <CorbadoProvider
-      projectId={process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID}
-      darkMode="off"
-      customTranslations={corbadoTranslations}
-    >
-      <Tabs
-        aria-label="Options"
-        className="justify-center mt-4"
-        selectedKey={selected}
-        onSelectionChange={setSelected as (key: Key) => void}
+    <div className="grid justify-center">
+      <CorbadoProvider
+        projectId={process.env.NEXT_PUBLIC_CORBADO_PROJECT_ID}
+        darkMode="off"
+        customTranslations={corbadoTranslations}
       >
-        <Tab key="login" title="Logg inn">
-          <Login onLoggedIn={closeModal} />
-        </Tab>
-        <Tab key="register" title="Registrer">
-          <SignUp onSignedUp={closeModal} />
-        </Tab>
-        <Tab key="social" title="Sosial">
-          <LoginOptions />
-        </Tab>
-      </Tabs>
-    </CorbadoProvider>
+        <Tabs
+          aria-label="Options"
+          className="justify-center mt-4"
+          selectedKey={selected}
+          onSelectionChange={setSelected as (key: Key) => void}
+        >
+          <Tab key="login" title="Logg inn">
+            <Login onLoggedIn={closeModal} />
+          </Tab>
+          <Tab key="register" title="Registrer">
+            <SignUp onSignedUp={closeModal} />
+          </Tab>
+          <Tab key="social" title="Sosial">
+            <Card>
+              <CardBody>
+                <LoginOptions />
+              </CardBody>
+            </Card>
+          </Tab>
+        </Tabs>
+      </CorbadoProvider>
+    </div>
   );
 }
