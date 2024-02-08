68a7006f15358661eb3e1d39e3e1eba818e65079
John Føreland <john.foreland@me.com>
2024-02-08 19:56:21 +0100

    added all login methods to login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
+import AuthCorbado from '@/components/auth/auth-corbado';
 import LoginOptions from '@/components/auth/login-options';
 
 export default function LoginPage() {
   return (
     <div className="mt-5">
-      <LoginOptions />
+      <AuthCorbado mode="login" />
     </div>
   );
 }
