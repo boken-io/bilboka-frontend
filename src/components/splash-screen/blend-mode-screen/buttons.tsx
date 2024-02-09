a2be45c410a0440549c0afa9af2a3dd74cc73acd
John Føreland <john.foreland@me.com>
2024-02-09 18:07:17 +0100

    emojis on front page

--- src/components/splash-screen/blend-mode-screen/buttons.tsx
+++ src/components/splash-screen/blend-mode-screen/buttons.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { LoginIcon, RegisterIcon } from '@/components/icons/icons';
 import { useRouter } from 'next/navigation';
 
 export default function Buttons({ user }: { user: any }) {
@@ -9,25 +10,28 @@ export default function Buttons({ user }: { user: any }) {
   return (
     <div className="grid grid-cols-3 mx-4 gap-4 place-self-center">
       <button
-        className="bg-white hover:bg-gray-200 text-black font-bold rounded-full py-2 px-4"
+        className="bg-white hover:bg-gray-200 text-black font-bold rounded-full py-2 px-4 grid grid-cols-1 justify-items-center"
         style={{ mixBlendMode: 'difference' }}
         onClick={() => router.push('?login=true', { scroll: false })}
       >
+        <span>🔐</span>
         Logg inn
       </button>
       <button
-        className="bg-white hover:bg-gray-200 text-black font-bold rounded-full py-2 px-4"
+        className="bg-white hover:bg-gray-200 text-black font-bold rounded-full py-2 px-4 grid grid-cols-1 justify-items-center"
         style={{ mixBlendMode: 'difference' }}
         onClick={() => router.push('?register=true', { scroll: false })}
       >
+        <span>✍️</span>
         Registrer
       </button>
       <button
-        className="bg-white hover:bg-gray-200 text-black font-bold rounded-full py-2 px-4"
+        className="bg-white hover:bg-gray-200 text-black font-bold rounded-full py-2 px-4 grid grid-cols-1 justify-items-center"
         style={{ mixBlendMode: 'difference' }}
         onClick={() => router.push('?sample=true', { scroll: false })}
       >
-        🛞 Prøvekjør
+        <span>🛞</span>
+        Prøvekjør
       </button>
     </div>
   );
